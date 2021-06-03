fe8fdc6019a5cfe8fff8b974c89c66b808fbc17c
Raleigh Wayland <raleigh.wayland@stormapp.com>
2021-06-02 22:56:45 -0600

    Adding project links

--- src/components/DevPortfolio.tsx
+++ src/components/DevPortfolio.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
 const DevPortfolio = () => {
-  return <div>Dev Portfolio</div>;
+  return (
+    <div>
+      <h1>Projects</h1>
+      <h2>
+        <a
+          href="https://photo.raleighwayland.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Photo Reference App
+        </a>
+      </h2>
+      <h2>
+        <a
+          href="https://synth.raleighwayland.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Browser Synth
+        </a>
+      </h2>
+      <h2>
+        <a href="https://ui.stormapp.com/" target="_blank" rel="noreferrer">
+          UI Library
+        </a>
+      </h2>
+    </div>
+  );
 };
 
 export default DevPortfolio;
