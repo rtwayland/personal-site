810334c4fa66d06eea9b670224caaefc7740c604
Raleigh Wayland <rtwayland@gmail.com>
2025-12-04 11:39:05 -0700

    Adding Cooper font

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -21,17 +21,6 @@ export default defineConfig({
 		meta: {
 			description: "Raleigh Wayland's Personal Website",
 		},
-		tags: [
-			{ tag: 'link', attrs: { rel: 'preconnect', href: 'https://fonts.googleapis.com' } },
-			{ tag: 'link', attrs: { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' } },
-			{
-				tag: 'link',
-				attrs: {
-					rel: 'stylesheet',
-					href: 'https://fonts.googleapis.com/css2?family=Crimson+Pro:ital,wght@0,200..900;1,200..900&display=swap',
-				},
-			},
-		],
 	},
 	output: {
 		distPath: 'build',
